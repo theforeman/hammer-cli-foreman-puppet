498d8ea01265cb0c1de1b1a3d32255c0ac7ded9e
Nadja Heitmann <nadjah@atix.de>
2021-05-05 14:52:48 +0200

    Move to loadable state

--- lib/hammer_cli_foreman_puppet/associating_commands/associating_commands.rb
+++ lib/hammer_cli_foreman_puppet/associating_commands/associating_commands.rb
@@ -2,11 +2,12 @@
 module HammerCLIForemanPuppet
   module AssociatingCommands
     module PuppetEnvironment
-      extend CommandExtension
+      extend HammerCLIForeman::AssociatingCommands::CommandExtension
 
-      class AddPuppetEnvironmentCommand < HammerCLIForeman::AddAssociatedCommand
+      class AddPuppetEnvironmentCommandPuppet < HammerCLIForeman::AddAssociatedCommand
         associated_resource :environments
         desc _('Associate a Puppet environment')
+        command_name "puppet_add-environment"
 
         success_message _("The environment has been associated.")
         failure_message _("Could not associate the environment")
@@ -14,9 +15,10 @@ module HammerCLIForemanPuppet
         extend_with(HammerCLIForemanPuppet::CommandExtensions::PuppetEnvironment.new)
       end
 
-      class RemovePuppetEnvironmentCommand < HammerCLIForeman::RemoveAssociatedCommand
+      class RemovePuppetEnvironmentCommandPuppet < HammerCLIForeman::RemoveAssociatedCommand
         associated_resource :environments
         desc _('Disassociate a Puppet environment')
+        command_name "puppet_remove-environment"
 
         success_message _("The environment has been disassociated.")
         failure_message _("Could not disassociate the environment")
