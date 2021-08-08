4ea358ee428cbf3e0f944e6266bc1d0cdb55d9a0
Amir Fefer <amirfefer@gmail.com>
2021-08-08 23:40:42 +0300

    Fixes #21 - add puppet env to combination commands

--- lib/hammer_cli_foreman_puppet/combination.rb
+++ lib/hammer_cli_foreman_puppet/combination.rb
@@ -1,13 +1,36 @@
 
 require 'hammer_cli_foreman/combination'
+require 'hammer_cli_foreman_puppet/command_extensions/combination'
 
 module HammerCLIForemanPuppet
+  class Combination < HammerCLIForemanPuppet::Command
+    class ListCommand < HammerCLIForemanPuppet::ListCommand
+      output do
+        field nil, _("Puppet Environment"), Fields::SingleReference, :key => :environment
+      end
+    end
+
+    class InfoCommand < HammerCLIForemanPuppet::InfoCommand
+      include EnvironmentNameMapping
+      output ListCommand.output_definition do
+        field :environment_id, _('Puppet Environment ID')
+        field :environment_name, _('Puppet Environment name')
+      end
+    end
+  end
+
+  HammerCLIForeman::Combination::ListCombination.extend_with(
+    HammerCLIForemanPuppet::CommandExtensions::ListCombination.new
+  )
   HammerCLIForeman::Combination::InfoCombination.extend_with(
+    HammerCLIForemanPuppet::CommandExtensions::InfoCombination.new,
     HammerCLIForemanPuppet::CommandExtensions::PuppetEnvironment.new
   )
+  HammerCLIForeman::Combination::UpdateCombination.include(HammerCLIForemanPuppet::EnvironmentNameMapping)
   HammerCLIForeman::Combination::UpdateCombination.extend_with(
     HammerCLIForemanPuppet::CommandExtensions::PuppetEnvironment.new
   )
+  HammerCLIForeman::Combination::CreateCombination.include(HammerCLIForemanPuppet::EnvironmentNameMapping)
   HammerCLIForeman::Combination::CreateCombination.extend_with(
     HammerCLIForemanPuppet::CommandExtensions::PuppetEnvironment.new
   )
---
+++ lib/hammer_cli_foreman_puppet/command_extensions/combination.rb
@@ -0,0 +1,15 @@
+module HammerCLIForemanPuppet
+  module CommandExtensions
+    class ListCombination < HammerCLI::CommandExtensions
+      output do |definition|
+        definition.insert(:after, :hostgroup, HammerCLIForemanPuppet::Combination::ListCommand.output_definition.fields)
+      end
+    end
+
+    class InfoCombination < HammerCLI::CommandExtensions
+        output do |definition|
+          definition.insert(:after, :hostgroup_name, HammerCLIForemanPuppet::Combination::InfoCommand.output_definition.fields)
+        end
+      end
+  end
+end
